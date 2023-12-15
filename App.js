2ec9fe1a48a821f2266499bed7a4b7531e4d07b6
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-15 20:24:41 +0430

    set up

--- App.js
+++ App.js
@@ -10,7 +10,12 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-         
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        </a>
       </header>
     </div>
   );
