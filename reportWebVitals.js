fe0d0e78f8904450cd3e10a7eef1b4f90880c948
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-16 20:27:59 +0430

    set up

--- reportWebVitals.js
+++ reportWebVitals.js
@@ -2,7 +2,7 @@ const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
-     
+      
     });
   }
 };
