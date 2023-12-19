d836cca713f925738f24873b5b05bc2c4c6ca3dc
MastooraTurkmen <janmastoorajan@gmail.com>
2023-12-19 21:01:05 +0430

    set up

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
+       
       </header>
     </div>
   );
