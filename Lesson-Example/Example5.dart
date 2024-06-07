a26b9253afe941151296abbd194984c32871e2a2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-07 14:47:08 +0430

    example

---
+++ Lesson-Example/Example5.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Collections in Dart: Lists, Sets, Maps
+  // List
+
+  // List<String> names = ['Fatema', 'Ahmad', 'Ali', 'Aalem', 'Elnaz'];
+  // names.add('Saber');
+  // print(names[4]);
+  List<int> numbers = [1, 2, 3, 4, 5];
+  numbers.add(10);
+  print(numbers[5]);
+}
