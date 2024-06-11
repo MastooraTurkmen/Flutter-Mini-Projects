7fe4fc7cc8ccc9d5c55206d8f331219e85a8cdd2
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-11 15:04:14 +0430

    for loops

--- Lesson-Example/Example6.dart
+++ Lesson-Example/Example6.dart
@@ -7,13 +7,34 @@ void main() {
     print('Student ${i + 1}: ${names[i]}');
   }
 
+  String text = 'Hello,World';
+  List<String> splittedText = text.split(',');
+
+  for (int i = 0; i < text.length; i++) {
+    print('$i: ${text[i]}');
+  }
+
   // For-In Loop
   for (String name in names) {
     print(name);
+  }
+
+  for (String char in splittedText) {
+    print('Character: $char');
   }
 
   // For Each Loop
   names.forEach((name) {
     print('Students: $name');
   });
+
+  splittedText.forEach((char) {
+    print('$char');
+  });
+
+  names.forEach(printName);
+}
+
+void printName(String name) {
+  print('For Each - Students: $name');
 }
