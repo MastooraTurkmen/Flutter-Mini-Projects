127b1aa2a81f8f0db2843e71e87d32e1be8efd6b
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-11 14:38:29 +0430

    for loop

--- Lesson-Example/Example6.dart
+++ Lesson-Example/Example6.dart
@@ -2,6 +2,7 @@ void main() {
   List<String> names = ['Fatema', 'Ahmad', 'Ali', 'Aalem', 'Elnaz'];
   names.add('Saber');
 
+  // For loop
   for (int i = 0; i < names.length; i++) {
     print('Student ${i + 1}: ${names[i]}');
   }
@@ -11,7 +12,7 @@ void main() {
     print(name);
   }
 
-  // Foor Each Loop
+  // For Each Loop
   names.forEach((name) {
     print('Students: $name');
   });
