4297df6282a88a69d5b19d16b52a06d62e677f53
MastooraTurkmen <janmastoorajan@gmail.com>
2024-06-29 17:50:16 +0430

    Objects

---
+++ 11-Objects-Classes.dart
@@ -0,0 +1,25 @@
+void main() {
+
+
+}
+
+class MyLaptop {
+  String? model;
+  String? brand;
+  int? ram;
+  double? size;
+
+  void displayDetails() {
+    print('The Brand of Laptop is: $brand');
+    print('The Model of Laptop is: $model');
+    print('The RAM of Laptop is RAM: ${ram}GB');
+    print('The Screen Size of Laptop is : ${size} inches');
+  }
+}
+
+class Student {
+  String? name;
+  int? age;
+  int? grade;
+  List<String>? subject;
+}
