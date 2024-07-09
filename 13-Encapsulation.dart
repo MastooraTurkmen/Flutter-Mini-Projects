2deafdee6fecaace913d0236386efed10e462d78
MastooraTurkmen <janmastoorajan@gmail.com>
2024-07-09 20:25:15 +0430

    encapsulation

--- 13-Encapsulation.dart
+++ 13-Encapsulation.dart
@@ -1,75 +1,88 @@
 void main() {
-  // Encapsulation: building data (variable)
-  // and method into a single class
-  BankAccount ba = BankAccount();
-  ba.balance = 200;
-  ba._accountNumber = '123';
-  print('balance: ${ba._balance}');
+  // The Student class
+  Student myStudent = Student('Shabnam', 23);
+  myStudent.addGrade('English', 69.0);
+  myStudent.addGrade('Math', 93.0);
+  // print them
+  print('Shabnam Grade in English: ${myStudent.getGrade('English')}');
+  print('The Average Grade is: ${myStudent.getAverageGrade()}');
+  print('The Student name: ${myStudent.name}');
+  print('The Student age: ${myStudent.age}');
 
-  // Animal cat = Cat();
+  // The Book class
+  Book myBook = Book('Say my Name', 'Kamal Khan', 14.03);
+  print(myBook.getBookDetails());
+  myBook.applyDiscount(5);
+  print('The price book after discount: ${myBook.getBookDetails()}');
+  myBook.price = 12.44;
+  print('The book updated price: ${myBook.getBookDetails()}');
 }
 
-class BankAccount {
-  // Field
-  String? _accountNumber;
-  double? _balance;
+class Student {
+  // Private fields
+  String _name;
+  int _age;
+  Map<String, double> _grades;
 
-  // double? get balance => _balance;
-  String? get accountNumber => _accountNumber;
+  // Constructor
+  Student(this._name, this._age) : _grades = {};
 
-  //setter for balance
-  set balance(double amount) {
-    if (amount >= 0) {
-      _balance = amount;
-    } else {
-      print('Amount cannot be negative');
-    }
+  // Public methods
+  void addGrade(String subject, double grade) {
+    _grades[subject] = grade;
   }
 
-  set accountNumber(String? accNumber) {
-    if (accNumber!.isNotEmpty) {
-      _accountNumber = accNumber;
-    } else {
-      print('Account number can\'t be');
-    }
+  double? getGrade(String subject) {
+    return _grades[subject];
   }
 
-  //Constructor for initilizing variables/properties
-
-  //Methods
-  void deposit(double amount) {
-    if (amount > 0) {
-      _balance = _balance! + amount;
-      print('Deposit successful. New Balance: $_balance');
-    } else {
-      print('Deposit amount must be positive');
+  double getAverageGrade() {
+    if (_grades.isEmpty) {
+      return 0.0;
     }
+    double total = _grades.values.reduce((a, b) => a + b);
+    return total / _grades.length;
   }
 
-  void withDraw(double amount) {
-    if (amount > 0 && amount <= _balance!) {
-      _balance = _balance! - amount;
-      print('Withdrawal was succesful. New Balance $_balance');
-    } else {
-      print('Invalid withdrawal amount');
-    }
-  }
+  // Getter and Setter methods for name and age
+  String get name => _name;
+  set name(String name) => _name = name;
+
+  int get age => _age;
+  set age(int age) => _age = age;
 }
 
-// polymorphism: superclass object: object of supar class and object sub
-//calss: Method overriding
+class Book {
+  // Private fields
+  String _title;
+  String _author;
+  double _price;
 
-class Animal {
-  void makeSound() {
-    print('General Animal Sound');
+  // Constructor
+  Book(this._title, this._author, this._price);
+
+  // Getter methods
+  String get title => _title;
+  String get author => _author;
+  double get price => _price;
+
+  // Setter method for price
+  set price(double price) {
+    if (price < 0) {
+      throw ArgumentError('Price should not be negative');
+    }
+    _price = price;
+  }
+
+  // Additional methods
+  void applyDiscount(double discountPercentage) {
+    if (discountPercentage < 0 || discountPercentage > 100) {
+      throw ArgumentError('The discount should be between 0 & 100');
+    }
+    _price -= _price * (discountPercentage / 100);
+  }
+
+  String getBookDetails() {
+    return 'Title: $_title, Author: $_author, Price: \$$_price';
   }
 }
-
-class Cat extends Animal {
-  void makeSound() {
-    print('Meow');
-  }
-}
-
-
-// Abstraction
